710ed4b8ea90aa519ffe3ea0579d3046dffcad58
Anirudhram2006 <anirudhram2k6@gmail.com>
2025-08-15 08:07:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,25 @@
-// Name of the cache
 const CACHE_NAME = "rationright-cache-v1";
-
-// Files to cache
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/manifest.json",
-  "/assets/icon-192.png",
-  "/assets/icon-512.png",
-  "/assets/fingerprint.png",
-  "/assets/grains1.jpg"
+  "/RationRight/",
+  "/RationRight/index.html",
+  "/RationRight/styles.css",
+  "/RationRight/app.js",
+  "/RationRight/manifest.json",
+  "/RationRight/assets/fingerprint.png",
+  "/RationRight/assets/eye.png",
+  "/RationRight/assets/grains1.jpg",
+  "/RationRight/assets/icon-192.png",
+  "/RationRight/assets/icon-512.png"
 ];
 
-// Install event - caching files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch event - serving cached content when offline
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
